a2b0ea955d2cde46b437815c1b9014c5f239b071
suumino <78770211+suumino@users.noreply.github.com>
2021-04-01 14:49:25 +0900

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,28 @@
+public class helloworld {
+    public static void main(String[] args) {
+        // int a = 10;
+        // int b = 5;
+        // System.out.println(a+b);
+        // System.out.println(a-b);
+        // System.out.println(a*b);
+        // System.out.println(a/b);
+        // System.out.println(7 % 3);
+        // System.out.println(3 % 7);
+///////////////////////////////
+        int i = 0;
+        int j = 10;
+        i++;
+        j--;
+
+        System.out.println(i);
+        System.out.println(j);
+/////////////////////////////
+        int k = 0;
+        System.out.println(k++);
+        System.out.println(k);
+/////////////////////////////
+        int l = 0;
+        System.out.println(++l);
+        System.out.println(l);
+    }
+}
