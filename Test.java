ed790622e74e75ae828d27e0df324fe2885e02a2
suumino <78770211+suumino@users.noreply.github.com>
2021-04-12 17:31:58 +0900

    Add files via upload

--- Test.java
+++ Test.java
@@ -1,7 +1,34 @@
-import java.util.Scanner;
 public class Test {
+    public void sayNick(String nick) throws FoolException {
+        if("fool".equals(nick)) {
+            throw new FoolException();
+        }
+        System.out.println("your nickname is "+nick);
+    }
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.next());
+        Test test = new Test();
+        try {
+            test.sayNick("fool");
+            test.sayNick("genious");
+        }catch(FoolException e) {
+            System.err.println("FoolException");
+        }
     }
 }
+    // public void shouldBeRun() {
+    //     System.out.println("ok, thanks.");
+    // }
+    // public static void main(String[] args) {
+    //     Test test = new Test();
+    //     int c;
+    //     try {
+    //         c = 4/0;
+    //         test.shouldBeRun();
+    //     } catch (ArithmeticException e) {
+    //         c = -1;
+    //     } finally {
+    //         test.shouldBeRun();
+    //     }
+    // }
+// }
